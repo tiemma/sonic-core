0cfdd91daf9572056538675dcb8d4eb685df2831
Emmanuel Bakare <bakareemmanuel98@outlook.com>
2020-11-20 11:14:39 +0100

    Fix issue with logger  being null

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,4 @@
 const debug = require('debug');
-const { dirname, relative } = require('path');
 const { name } = require('../package.json');
 
 const debugLogger = (namespace) => debug(`${name}: ${namespace}`);
