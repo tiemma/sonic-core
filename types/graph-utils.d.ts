ec29456e3146a5125d24d9f9638488bf30ef9bce
Emmanuel Bakare <bakareemmanuel98@outlook.com>
2021-08-04 20:35:21 +0100

    add optional flag to request object

--- types/graph-utils.d.ts
+++ types/graph-utils.d.ts
@@ -1,15 +1,15 @@
-import { RequestObject} from "./swagger-utils";
+import { RequestObject } from './swagger-utils';
 
 export type Node = string;
 
 export interface Dependency {
     dependencies: Node[],
-    requestData: RequestObject
+    requestData?: RequestObject
 }
 
 export type DependencyGraph = Record<Node, Dependency>
 
 export function satisfyDependencyConstraints(dependencyGraph: DependencyGraph): void;
 export function dependencyCycleDetection(dependencyGraph: DependencyGraph): void;
-export function topologicalDependencySort(dependencyGraph: DependencyGraph): import("./structures");
-//# sourceMappingURL=graph-utils.d.ts.map
+export function topologicalDependencySort(dependencyGraph: DependencyGraph): import('./structures');
+// # sourceMappingURL=graph-utils.d.ts.map
