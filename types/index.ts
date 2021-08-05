ed3ecb1c426e76084ff2e59ac33646b46cce7e01
Emmanuel Bakare <bakareemmanuel98@outlook.com>
2021-08-05 10:00:44 +0100

    export Queue

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,7 @@
+import Queue from './structures';
+
 export * from './api-utils';
 export * from './graph-utils';
 export * from './regex-utils';
 export * from './swagger-utils';
+export { Queue };
